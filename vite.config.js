af22b6694d8e29973abb638ace38402d579b3e99
00146520 <00146520@uca.edu.sv>
2023-07-07 14:36:13 -0600

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://Wi1S0rt0.github.io/Toptrend-deploy/'
 })
